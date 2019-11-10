a29f90f6828f545f554996bc8cea1a6b231d8474
OMOTOLA OLAJIDE <jideomotola33@gmail.com>
2019-11-10 21:40:07 +0100

    random walk with axis turned off

--- rw_visual.py
+++ rw_visual.py
@@ -7,11 +7,18 @@ while True:
     #Make a random walk, and plot the points.
     rw = RandomWalk()
     rw.fill_walk()
-    
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                 edgecolor='none', s=15)
-    plt.savefig('Random walk with the Blue colormap')
+    
+    #Emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+                s=100)
+    
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+    plt.show('Random walk with the Blue colormap and start_end point')
     
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
